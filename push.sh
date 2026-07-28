bef80157c48bf4150e5b3d45e362dd3e24f76434
huangli123456 <1121143386@qq.com>
2026-07-28 16:22:17 +0800

    添加示例数据

---
+++ push.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# GitHub推送脚本
+
+echo "正在推送代码到GitHub..."
+
+# 进入项目目录
+cd "/Users/huangli/CodeBuddy/20260728132626/个人工作台"
+
+# 推送代码
+git push -u origin main
+
+echo "推送完成！"
+echo "仓库地址：https://github.com/huangli123456/hl-workbench"
